d5e1afbab5236b200183730407907a9bd188a069
Igor Lucas <igorescritor@hotmail.com>
2021-06-08 13:31:47 -0300

    Commit inicial do projeto

---
+++ API_Restful_Spring_MongoDB/src/main/java/com/igorlucas/ApiRestfulSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.igorlucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringMongoDbApplication.class, args);
+	}
+
+}
