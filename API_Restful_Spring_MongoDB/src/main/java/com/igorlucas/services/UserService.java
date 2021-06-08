c69e2daed8fdfcbeb4c6b838ba6b59c63427cc86
Igor Lucas <igorescritor@hotmail.com>
2021-06-08 20:55:29 -0300

    Inserindo usuários com o POST

--- API_Restful_Spring_MongoDB/src/main/java/com/igorlucas/services/UserService.java
+++ API_Restful_Spring_MongoDB/src/main/java/com/igorlucas/services/UserService.java
@@ -7,22 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.igorlucas.domain.User;
+import com.igorlucas.dto.UserDTO;
 import com.igorlucas.repository.UserRepository;
 import com.igorlucas.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
 
 }
